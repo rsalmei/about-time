fd78b2b44c09620a62a664d7ee0018aac48db122
Rogério Sampaio <rsalmei@gmail.com>
2021-01-10 16:58:31 -0300

    bump version

--- about_time/__init__.py
+++ about_time/__init__.py
@@ -6,7 +6,7 @@ import sys
 from .core import about_time
 from .human import duration_human, throughput_human
 
-VERSION = (3, 1, 0)
+VERSION = (3, 1, 1)
 
 __author__ = 'Rogério Sampaio de Almeida'
 __email__ = 'rsalmei@gmail.com'
