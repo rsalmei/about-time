9e4b57a1c9902f350978083b9168b3cfee0c4496
Rogério Sampaio <rsalmei@gmail.com>
2022-04-28 04:29:13 -0300

    new count and throughput with three significant digits and auto SI-prefixes

--- about_time/human.py
+++ about_time/human.py
@@ -37,24 +37,59 @@ def duration_human(value):
 
 
 THROUGHPUT_HUMAN_SPEC = (
-    (1. / 60 / 24, 60 * 60 * 24, 24, '/d'),
-    (1. / 60, 60 * 60, 60, '/h'),
-    (1., 60, 60, '/m'),
-    (float('inf'), 1, float('inf'), '/s'),
+    (1. / 60 / 24, 60 * 60 * 24, 24., '/d'),
+    (1. / 60, 60 * 60, 60., '/h'),
+    (1., 60, 60., '/m'),
+    # /s in code.
 )
 
 
-def throughput_human(value):
-    """Return a beautiful representation of the current throughput.
+def throughput_human(value, what=''):
+    """Return a beautiful representation of some throughput.
     It dynamically calculates the best unit to use.
 
+    Args:
+        value (float): value to be formatted
+
     Returns:
-        str: the duration representation.
+        str: the human friendly representation.
 
     """
-    for top, mult, size, unit in THROUGHPUT_HUMAN_SPEC:
-        if value < top:
-            result = round(value * mult, ndigits=2)
-            if result < size:
-                return '{}{}'.format(result, unit)
-    return '?'
+    enter = next((i for i, s in enumerate(THROUGHPUT_HUMAN_SPEC) if value < s[0]), 99)
+    if enter != 99:
+        value *= THROUGHPUT_HUMAN_SPEC[enter][1]
+
+    for _, _, size, unit in THROUGHPUT_HUMAN_SPEC[enter:]:
+        if value < 9.995:
+            return '{:1.2f}{}{}'.format(value, what, unit)
+        if value < size - .05:
+            return '{:2.1f}{}{}'.format(value, what, unit)
+        value /= size
+
+    return '{}/s'.format(count_human(value, what))
+
+
+COUNT_HUMAN_SPEC = ('', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+
+
+def count_human(value, what='', divisor=1000):
+    """Return a beautiful representation of some count.
+    It dynamically calculates the best unit to use.
+
+    Args:
+        value (float): value to be formatted
+
+    Returns:
+        str: the human friendly representation.
+
+    """
+    for unit in COUNT_HUMAN_SPEC:
+        if value < 9.995:
+            return '{:1.2f}{}{}'.format(value, unit, what)
+        if value < 99.95:
+            return '{:2.1f}{}{}'.format(value, unit, what)
+        if value < 999.5:
+            return '{:3.0f}{}{}'.format(value, unit, what)
+        value /= divisor
+
+    return '{:3}Y+{}'.format(value, what)
