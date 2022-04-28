0d90aa6d5bc3ae5b7816326c06d64f119484255b
Rogério Sampaio <rsalmei@gmail.com>
2022-04-28 04:27:20 -0300

    new three significant digits implementation

--- about_time/human.py
+++ about_time/human.py
@@ -1,32 +1,39 @@
 from datetime import timedelta
 
 DURATION_HUMAN_SPEC = (
-    (1.e-6, 1e9, 1e3, 'ns'),
-    (1.e-3, 1e6, 1e3, 'us'),
-    (1., 1e3, 1e3, 'ms'),
-    (60., 1e0, 60., 's'),
+    (1.e-6, 1.e9, 1.e3, 'ns'),
+    (1.e-3, 1.e6, 1.e3, 'µs'),
+    (1., 1.e3, 1.e3, 'ms'),
+    (60., 1., 1., 's'),  # size=1 because it won't change the unit.
+    # 00:01:00 and beyond in code.
 )
 
 
 def duration_human(value):
-    """Return a beautiful representation of the duration.
+    """Return a beautiful representation of some duration.
     It dynamically calculates the best unit to use.
 
+    Args:
+        value (float): value to be formatted
+
     Returns:
-        str: the duration representation.
+        str: the human friendly representation.
 
     """
-    for top, mult, size, unit in DURATION_HUMAN_SPEC:
-        if value < top:
-            result = round(value * mult, ndigits=2)
-            if result < size:
-                return '{}{}'.format(result, unit)
+    enter = next((i for i, s in enumerate(DURATION_HUMAN_SPEC) if value < s[0]), 99)
+    if enter != 99:
+        value *= DURATION_HUMAN_SPEC[enter][1]
 
-    txt = str(timedelta(seconds=float('{:.1f}'.format(value))))
-    pos = txt.find('.')
-    if pos == -1:
-        return txt
-    return txt[:pos + 2]
+    for _, _, size, unit in DURATION_HUMAN_SPEC[enter:]:
+        if value < 9.995:
+            return '{:1.2f}{}'.format(value, unit)
+        if value < 99.95:
+            return '{:2.1f}{}'.format(value, unit)
+        if value < 999.5:
+            return '{:3.0f}{}'.format(value, unit)
+        value /= size
+
+    return str(timedelta(seconds=round(value, 0)))
 
 
 THROUGHPUT_HUMAN_SPEC = (
