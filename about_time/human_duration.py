55d17768f4cbeb35bcf3ba611aa4eac42291bf62
Rogério Sampaio <rsalmei@gmail.com>
2022-12-19 23:46:39 -0300

    support for fixed precision

--- about_time/human_duration.py
+++ about_time/human_duration.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from .features import FEATURES, conv_space
 
 SPEC = (
@@ -10,33 +12,43 @@ SPEC = (
 )
 
 
-def __human_duration(val: float, space: str):
+def __human_duration(val: float, prec: Optional[int], space: str) -> str:
     val *= 1e9
     for size, div_next, scale, dec in SPEC:
         r = round(val, dec)
         if r >= size:
             val /= div_next
+            continue
+
+        if prec is not None:
+            pass
         elif r % 1. == 0.:
-            return '{:.0f}{}{}'.format(r, space, scale)
+            prec = 0
         elif (r * 10.) % 1. == 0.:
-            return '{:.1f}{}{}'.format(r, space, scale)
+            prec = 1
         else:
-            return '{:.2f}{}{}'.format(r, space, scale)
+            prec = 2
+        return '{:.{}f}{}{}'.format(r, prec, space, scale)
 
     val = round(val, 1)
     m = val / 60.
     if m < 60.:
         r = val % 60.
-        if r % 1. == 0.:
+        if prec is not None:
+            pass
+        elif r % 1. == 0.:
+            prec = 0
+
+        if prec == 0:
             return '{:.0f}:{:02.0f}'.format(m // 1., r)
         return '{:.0f}:{:04.1f}'.format(m // 1., round(r, 1))
-    else:
-        return '{:.0f}:{:02.0f}:{:02.0f}'.format(m / 60. // 1., m % 60. // 1., val % 60. // 1.)
+
+    return '{:.0f}:{:02.0f}:{:02.0f}'.format(m / 60. // 1., m % 60. // 1., val % 60. // 1.)
 
 
 def fn_human_duration(space: bool):
-    def run(val):
-        return __human_duration(val, space)
+    def run(val, prec: Optional[int]):
+        return __human_duration(val, prec, space)
 
     space = conv_space(space)
     return run
@@ -51,15 +63,18 @@ class HumanDuration(object):
     def value(self):
         return self._value
 
-    def as_human(self) -> str:
+    def as_human(self, prec: Optional[int] = None) -> str:
         """Return a beautiful representation of this duration.
         It dynamically calculates the best scale to use.
+
+        Args:
+            prec: an optional custom precision
 
         Returns:
             the human friendly representation.
 
         """
-        return fn_human_duration(FEATURES.feature_space)(self._value)
+        return fn_human_duration(FEATURES.feature_space)(self._value, prec)
 
     def __str__(self):
         return self.as_human()
