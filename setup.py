102648073bf553157ae82edd6bc7acd6437f68af
Rogério Sampaio <rsalmei@gmail.com>
2022-12-21 01:12:21 -0300

    include 3.11 in pypi (but support came in 4.2.0 actually, with the github action check)

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     keywords='python track tracker time code blocks monitor statistics analytics'.split(),
     packages=find_packages(),
